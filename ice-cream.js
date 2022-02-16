8395b5fd3a6f82abed64a80239af3018ab118c44
mariannebourcier <marianne.bourcier@gmail.com>
2022-02-16 13:47:09 -0700

    ice-cream.js

---
+++ ice-cream.js
@@ -0,0 +1,5 @@
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+iceCreamFlavours.push("rootbeer");
+console.log(iceCreamFlavors[0]);
